8c77c77c48d5f15d08a26dd1dbee8925a25587c5
Michael Comella <michael.l.comella@gmail.com>
2017-11-29 17:06:35 -0800

    initial commit: List files in data and cache dirs.

---
+++ app/src/main/java/xyz/mcomella/whatsinmydatadirectory/MainActivity.kt
@@ -0,0 +1,35 @@
+package xyz.mcomella.whatsinmydatadirectory
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.File
+
+private const val LOGTAG = "DataDirContents"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        logDirectory(filesDir)
+        logDirectory(cacheDir)
+    }
+}
+
+private fun log(msg: String) {
+    Log.d(LOGTAG, msg)
+}
+
+private fun logDirectory(dir: File) {
+    if (!dir.exists() || !dir.isDirectory) throw IllegalStateException("Expected ${dir.absolutePath} to be a directory.")
+
+    log("--- ${dir.absolutePath} ---")
+    dir.list().forEach { log("  $it") }
+    log("") // newline.
+}
